533ffffe0bba4aae51664839093695efa5f54213
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-06 12:01:07 +0530

    Create Data Types.py

---
+++ 1. Python/1. Python Tutorial/4. Data Types and User Input/Data Types.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+int  = 12
+float = 12.3
+
+multiply = int * float
+print(multiply)
+
+boolean = True
+Binary = 1
+range = range(1,10)
+
+string = "Hello World"
+
+tuple = (1,2,3)
+
+list = [1,2,3]
+
+set = {1,2,3}
+
+dict = {"key": "value"}
+
+print(int)
+print(list)
+print(set)
+print(dict)
