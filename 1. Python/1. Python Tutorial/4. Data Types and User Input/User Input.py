819a6ce169bed7b2735e3b3848e353fecaf39528
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-06 12:01:03 +0530

    Create User Input.py

---
+++ 1. Python/1. Python Tutorial/4. Data Types and User Input/User Input.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+name = input("Enter your name: ")
+print(name)
+
+# COMMAND ----------
+
+age = int(input("Enter your age: "))
+print(age)
+
+# COMMAND ----------
+
+floats = float(input("Enter your mark"))
+print(floats)
+
+# COMMAND ----------
+
+Evaluates = eval(input("Enter experession"))
+print(Evaluates)
+
+# COMMAND ----------
+
+Evaluates = eval(input("Enter experession"))
+print(Evaluates)
