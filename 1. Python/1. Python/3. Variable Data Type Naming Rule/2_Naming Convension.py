f17b88ba0cf9a263afa0b615ca619f9800c0a3a7
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-10 14:19:54 +0530

    Update 2_Naming Convension.py

--- 1. Python/1. Python/3. Variable Data Type Naming Rule/2_Naming Convension.py
+++ 1. Python/1. Python/3. Variable Data Type Naming Rule/2_Naming Convension.py
@@ -1,7 +1,23 @@
+# variable naming conventions an rules
+# letters, digits, and underscores
+# must start with _or letter
+# can't start with number
+# case sensitive
+# can't use python keyword
 
 name = "Ck"
-name = "Ck Ray"
+Name = "Ck Ray"
+name = "Chakresh" # reassign
 print(name)
 
 user_name = "comchakreshray"
 print(user_name)
+
+print = "Light" # error python in_build function
+print(print)
+
+# Variable naming conventions
+# Snake Case
+user_name = "Chak@ee11ee8"
+
+_name_my_name_ = "Ambreesh Kumar"
