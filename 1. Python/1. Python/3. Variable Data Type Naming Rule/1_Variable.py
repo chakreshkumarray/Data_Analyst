830d2e3ad3dd35925cd1d4216f65a9b0ec8b24f9
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-10 14:19:46 +0530

    Update 1_Variable.py

--- 1. Python/1. Python/3. Variable Data Type Naming Rule/1_Variable.py
+++ 1. Python/1. Python/3. Variable Data Type Naming Rule/1_Variable.py
@@ -1,16 +1,20 @@
+# Creating variable in python
+# name, value, memory location, data type
 x = 1
 print(x)
 print(type(x))
+# print(len(x))
 
-name = "Chakresh"
+name = "Chakresh" # String type
 print(name)
 print(type(name))
+print(len(name))
 
-price = 999.9
+price = 999.9 # float type
 print(price)
 print(type(price))
 
-is_adult = False
+is_adult = False # boolean
 print(is_adult)
 print(type(is_adult))
 
@@ -18,3 +22,9 @@ a = 12
 b = 13
 c = 14
 print(a,b,c)
+print(a,b,c, sep=" ") # hidden working
+print(a,b,c, sep=",")
+
+d, e,f, g = 3, 2, 9.9, "Hello"
+print(d, e, f, g, sep=",") 
+print(d + e)
