fd026d4276800ef8fbf4ecd58f2dc21f6ba44c99
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-11 14:05:21 +0530

    Update 10_Length.py

--- 1. Python/1. Python/4. String/10_Length.py
+++ 1. Python/1. Python/4. String/10_Length.py
@@ -3,6 +3,19 @@
 name = "Chakresh"
 print(len(name))
 
+# OR
+a = "Python"
+length = len(a)
+print(length)
+
 print("")
+print(len(""))
 
 print(len("Ck"))
+
+print(len("Hello\nWorld"))
+print(len("❤️")) # 2 len
+print(len("🙂")) # 1 len
+print("\u2764")
+print("\uFE0F")
+print("\u2764\uFE0F")
