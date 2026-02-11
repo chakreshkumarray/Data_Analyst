50b668efa6038ca1990c4242da1f99ac7c97a8d5
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-11 13:55:15 +0530

    Update 9_Star.py

--- 1. Python/1. Python/4. String/9_Star.py
+++ 1. Python/1. Python/4. String/9_Star.py
@@ -1,3 +1,8 @@
+# Star
+c = 2
+res = c * 3
+print(res)
+
 a = "Chakresh"
 res = a * 3
 print(res)
@@ -5,9 +10,10 @@ print(res)
 star = "*"
 print((star * 5 + "\n") * 5)
 
-b= "ck"
+b = "ck"
 print(b * 0)
 print(b)
 print(b * -1)
 
-print("py" * 2 + "thon" * 2)
+# PyPythonthon
+print("Py" * 2 + "thon" * 2)
