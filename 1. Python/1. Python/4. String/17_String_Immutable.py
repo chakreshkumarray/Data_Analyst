08a1fa3331ef158a843fd5b89780f79d91cfc795
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-12 15:28:02 +0530

    Update 17_String_Immutable.py

--- 1. Python/1. Python/4. String/17_String_Immutable.py
+++ 1. Python/1. Python/4. String/17_String_Immutable.py
@@ -1,10 +1,28 @@
+# String Immutable in Python
+
 s = "Hello"
-s = "Bye"
-s[0] = "M" # error not able to mutable
-print(s)
+print(id(s))
 
-rs = r"C:\Users\AppData\Downloads\Photos"
+print(s(0))
+
+# s[0] = "M" won't work
+print("M" + s[1:])
+s = "M" + s[1:]
+print(id(s))
+
+# Raw Strings
+rs = r"He\llo"
 print(rs)
+print(r"C:\Users\Name\Documents\Photos")
 
-#pop
-#Palindrome cdheck
+'''
+r"She said, \"Hello\""    # Works: backslash escapes the quote
+r"She said, "Hello""      # SyntaxError: quote ends the string too
+r"This ends with a \\"    # Works
+r"This ends with a \\\"   # SyntaxError
+r"This ends with a \"     # SyntaxError
+r"This ends with a \""    # Works
+'''
+
+# odd \ --> escape closing quote
+# even \ --> doesn't escape closing quote
