05a472912b156bbd5764232700296615be99bbc3
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-12 08:28:03 +0530

    Update 12_String_Indexing.py

--- 1. Python/1. Python/4. String/12_String_Indexing.py
+++ 1. Python/1. Python/4. String/12_String_Indexing.py
@@ -1,6 +1,20 @@
+
+# String Indexing --> finding the char from string
+
+txt = "Python"
+print(txt[len(txt)-1]) # find last character
+
 name = "Chakresh"
 print(len(name))
 #print(name[len(name)]) # error
 
 print(name[len(name)-1])
 print(name[-1])
+
+'''
+String:   P  Y  T  H  O  N
+Index:    0  1  2  3  4  5
+Neg Idx: -6 -5 -4 -3 -2 -1
+'''
+
+print(txt[-1])
