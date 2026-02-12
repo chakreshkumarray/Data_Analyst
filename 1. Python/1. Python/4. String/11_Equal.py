7c5ab1a3c1d0f23ccbc884b0cdc856b178174bad
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-12 08:27:57 +0530

    Update 11_Equal.py

--- 1. Python/1. Python/4. String/11_Equal.py
+++ 1. Python/1. Python/4. String/11_Equal.py
@@ -1,21 +1,37 @@
-#Equal Check
+# Equal Check
 msg = "Help!"
+res = len(msg) == 5
 res = len(msg) >= 5
 print(res)
 
-print("apple" == "apple")
+print("apple" == "apple") # Equal Operator
 print("apple" == "Apple")
 print("apple" == "orange")
-print("apple" != "orange")
-print(1 < 2)
-print(11 > 2)
-print(15 <= 8)
+print("apple" != "orange") 
+
+print(1 < 2)       # Greater than
+print(11 > 2)      # Less then
+print(15 <= 8)     
 print(23 >= 9)
-print("a" > "b")
+
+print("a" > "b")  # Lexgiocographically
 print("apple" < "banana")
 print("apple" < "ape")
+
 print(ord("a")) # 97
 print(ord("A")) # 65
 print(ord("C"))
 print(ord("c"))
 print(chr(99))
+print(chr(45))
+
+print("\u0061")
+
+print(ord("C"))
+print(ord("H"))
+print(ord("A"))
+print(ord("K"))
+print(ord("R"))
+print(ord("E"))
+print(ord("S"))
+print(ord("H"))
