dd11691ca165cecc99f62febe3fec62c944ed139
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-12 15:28:19 +0530

    Update 15_String_Format.py

--- 1. Python/1. Python/4. String/15_String_Format.py
+++ 1. Python/1. Python/4. String/15_String_Format.py
@@ -1,4 +1,7 @@
+# Format
+
 name = "Chakresh"
 age = "70"
 message = "Hello,My name is {} my age is ".format(name,age)
+message = "Hello,My name is {0} my age is {1}-{0} ".format(name,age)
 print(message)
