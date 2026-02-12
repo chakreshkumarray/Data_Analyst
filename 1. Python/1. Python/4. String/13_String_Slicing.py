15ac6289e2cb51b53aa262916eb57d5207a736a9
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-12 08:27:49 +0530

    Update 13_String_Slicing.py

--- 1. Python/1. Python/4. String/13_String_Slicing.py
+++ 1. Python/1. Python/4. String/13_String_Slicing.py
@@ -1,3 +1,7 @@
+# Slicing --> break String in small char 
+# start - inclusive
+# end - exclusive no include
+
 txt = "Chakresh Kumar"
 print(txt[0:1])
 print(txt[0:4])
@@ -13,4 +17,34 @@ print(txt[1:6:2])
 
 # traverse
 print(txt[::-2])
+print(txt[::-1])
+
+txt = "Python Programming"
+txt[0:5]
+slice = txt[0:6]
+print(slice)
+
+# Programming
+print(txt[0:6])
+
+'''
+P y t h o n   P r o g  r  a  m  m  i  n  g
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+'''
+# Programming
+print(txt[7:18])
+
+# Programming
+print(txt[7:])
+
+# Python Programming
+print(txt[:])
+
+# Pro
+print(txt[-11:-8])
+
+# Pto
+print(txt[:6:2])
+
+# Reverse String
 print(txt[::-1])
