a4ef033ac79a3a3d18401d6c3f30c96a61c95498
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-17 09:07:04 +0530

    Update 1_Integers.py

--- 1. Python/1. Python/5. Numbers/1_Integers.py
+++ 1. Python/1. Python/5. Numbers/1_Integers.py
@@ -1,3 +1,5 @@
+# positive, negative, zero
+
 a = 10
 b = -22
 c = 0
@@ -14,12 +16,21 @@ print(type(i))
 salary = 10_00_000
 print(salary)
 
-binary_num = 55
+binary_num = 101010
 print(bin(binary_num))
+
+print(bin(10))
+
 num = 0b101
 print(bin(num))
 print(type(num))
 print(num)
 
 hex_num = 0x2A
+print(hex_num)
+
+hex_num = 0x2AF
+2*256 + 160 + 15 
+512 + 160 + 15
+
 print(hex_num)
